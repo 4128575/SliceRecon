827450ad58174ed3efa9fcdde8b76756d8fb0087
4128575 <r.a.schoonhoven@students.uu.nl>
2019-10-24 15:29:43 +0200

    configuration parser and plugin to load and apply segmentation written

--- example/sr_plugin_msd_segm.py
+++ example/sr_plugin_msd_segm.py
@@ -12,6 +12,7 @@ def set_config(argv):
     -di --dilations, list of dilations as [..]
     -p --parallel, bool if want to use multiple GPUs or not
     -bs --batch_size, batch size
+    -pt --path, set path to trained network .../*.pytorch
     """
     ### Default values ###
     c_in = 1
@@ -21,30 +22,67 @@ def set_config(argv):
     dilations = [1,2,3,4,5,6,7,8,9,10]
     parallel=False  #multiple GPUs or not
     batch_size = 1
+    net_path = "/export/scratch2/schoonho/Planmeca/
+        trained_networks/msd_axial_jordi.pytorch"
     try:
-        opts, args = getopt.getopt(argv,"c:n:d:w:di:p:bs:",["cin=","num_labels=",
-            "depth=","width=","dilations=","parallel=","batch_size="])
+        opts, args = getopt.getopt(argv,"hc:n:d:w:di:p:bs:pt:",["help=","cin=",
+            "num_labels=","depth=","width=","dilations=","parallel=",
+            "batch_size=","path="])
     except getopt.GetoptError:
-        print('sr_plugin_thresholding.py -t <threshold>')
+        print('sr_plugin_thresholding.py -c <channels_in> -n <number_of_labels> -d <depth>
+                -w <width> -di <list_dilations> -p <parallel> -bs <batch_size>')
         sys.exit(2)
     for opt, arg in opts:
-        if opt in ("-t", "--threshold"):
-            thrs = np.float64(arg)
-        else:
-            print('sr_plugin_thresholding.py -t <threshold>')
+        if opt in ("-h", "--help"):
+            print('sr_plugin_thresholding.py -c <channels_in> -n <number_of_labels> 
+                    -d <depth> -w <width> -di <list_dilations> 
+                    -p <parallel> -bs <batch_size>')
             sys.exit()
-    return c_in, num_labels, depth, width, dilations, parallel, batch_size
+        elif opt in ("-c", "--cin"):
+            c_in = arg
+        elif opt in ("-n", "--num_labels"):
+            num_labels = arg
+        elif opt in ("-d", "--depth"):
+            depth = arg
+        elif opt in ("-w", "--width"):
+            width = arg
+        elif opt in ("-di", "--dilations"):
+            dilations = arg
+        elif opt in ("-p", "--parallel"):
+            parallel = arg
+        elif opt in ("-bs", "--batch_size"):
+            batch_size = arg
+        elif opt in ("-pt", "--path"):
+            net_path = arg
+        else:
+            print('sr_plugin_thresholding.py -c <channels_in> -n <number_of_labels> 
+                    -d <depth> -w <width> -di <list_dilations> 
+                    -p <parallel> -bs <batch_size>')
+            sys.exit()
+    return c_in, num_labels, depth, width, dilations, parallel, batch_size, net_path
 
-c_in, num_labels, depth, width, dilations, parallel, batch_size = set_config(sys.argv[1:])
+c_in, num_labels, depth, width, dilations, parallel, batch_size, net_path =
+    set_config(sys.argv[1:])
+
+def load_network(net_path):
+    model = mp.MSDSegmentationModel(c_in, num_labels, depth,
+            width, dilations=dilations, parallel=parallel)
+    model.load(net_path)
+    return model
+
+model = load_network(net_path)
 
 def callback(shape, xs, idx):
     xs = np.array(xs).reshape(shape)
-
     print("callback called", shape)
-    xs[xs <= threshold] = 0.0
-    xs[xs > threshold] = 10.0
-
-    return [shape, xs.ravel().tolist()]
+    output_slice = np.zeros(shape=shape)
+    model.forward(xs)
+    #TODO: Generalize for arbitrary segmentation with arguments given in command line
+    output_slice = model.output.detach().cpu()[0][1]
+    #TODO: Threshold segmentation at 0.5, also with cmd argument
+    #output_slice[output_slice <= threshold] = 0.0
+    #output_slice[output_slice > threshold] = 10.0
+    return [shape, output_slice.ravel().tolist()]
 
 p = slicerecon.plugin("tcp://*:5652", "tcp://localhost:5555")
 p.set_slice_callback(callback)
